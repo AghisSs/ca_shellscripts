a1192011800b9e9fdff77ed1832bfbded752a264
AghisSs <122785065+AghisSs@users.noreply.github.com>
2023-01-25 10:06:10 +0100

    Update basicstring.sh

--- basicstring.sh
+++ basicstring.sh
@@ -3,7 +3,6 @@
 BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really 
 long hill."
 
-    # write your code here
     ISAY=$BUFFETT
     change1=${ISAY[@]/snow/foot}
     change2=${change1[@]//snow/}
@@ -11,7 +10,6 @@ long hill."
     loc=`expr index "$change3" 'w'`
     ISAY=${change3::$loc+2}
 
-# Test code - do not modify
 echo "Warren Buffett said:"
 echo $BUFFETT
 echo "and I say:"
